1e95b760dfe638c03e5c5acab465387a3f09d738
vitormuuniz <vitormuuniz@gmail.com>
2020-07-08 14:11:26 -0300

    refactoring suite class to run the test classes with junit5

--- backend/src/test/java/br/com/hostel/suite/DataApplicationTests.java
+++ backend/src/test/java/br/com/hostel/suite/DataApplicationTests.java
@@ -1,28 +1,9 @@
 package br.com.hostel.suite;
 
-import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
-import br.com.hostel.tests.get.CustomerGetTests;
-import br.com.hostel.tests.get.ReservationGetTests;
-import br.com.hostel.tests.get.RoomGetTests;
-import br.com.hostel.tests.post.CustomerPostAndDeleteTests;
-import br.com.hostel.tests.post.ReservationPostAndDeleteTests;
-import br.com.hostel.tests.post.RoomPostAndDeleteTests;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-        ReservationPostAndDeleteTests.class,
-        ReservationGetTests.class,
-        CustomerPostAndDeleteTests.class,
-        CustomerGetTests.class,
-        RoomPostAndDeleteTests.class,
-        RoomGetTests.class
-})
-public class DataApplicationTests {
-	
-    @Test
-    public void contextLoads() {}
-    
-}
+@RunWith(JUnitPlatform.class)
+@SelectPackages("br.com.hostel.tests")
+public class DataApplicationTests {}
