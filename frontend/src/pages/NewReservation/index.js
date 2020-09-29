756707b7b41c660be5fde6c87c96c06d74834e00
vitormuuniz <vitormuuniz@gmail.com>
2020-09-28 21:59:20 -0300

    adding form validation

--- frontend/src/pages/NewReservation/index.js
+++ frontend/src/pages/NewReservation/index.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
+import * as Yup from "yup";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+
 import api from "../../services/api";
 import "./styles.css";
 
@@ -14,19 +17,19 @@ export default function NewReservation() {
   const [minDailyRate, setMinDailyRate] = useState("");
   const [maxDailyRate, setMaxDailyRate] = useState("");
 
-
   const token = localStorage.getItem("token");
 
   const history = useHistory();
 
   async function handleRegister(e) {
+    console.log(checkinDate);
+    console.log(checkoutDate);
     e.preventDefault();
 
     try {
-
-      localStorage.setItem('checkinDate', checkinDate);
-      localStorage.setItem('checkoutDate', checkoutDate);
-      localStorage.setItem('numberOfGuests', numberOfGuests);
+      localStorage.setItem("checkinDate", checkinDate);
+      localStorage.setItem("checkoutDate", checkoutDate);
+      localStorage.setItem("numberOfGuests", numberOfGuests);
 
       await api.get("api/rooms", {
         headers: { Authorization: "Bearer " + token },
@@ -36,9 +39,8 @@ export default function NewReservation() {
           numberOfGuests,
           minDailyRate,
           maxDailyRate,
-        }
+        },
       });
-
 
       history.push("/room/selectAvailableRooms");
     } catch (err) {
@@ -61,45 +63,77 @@ export default function NewReservation() {
           </Link>
         </section>
 
-        <form onSubmit={handleRegister}>
-          <label> Check-in</label>
-          <input
-            required="true"
-            id="check-in"
-            type="date"
-            placeholder="Check-in"
-            value={checkinDate}
-            onChange={(e) => setCheckinDate(e.target.value)}
-          />
-          <label>Check-out</label>
-          <input
-            required="true"
-            id="check-out"
-            type="date"
-            placeholder="Check-out"
-            value={checkoutDate}
-            onChange={(e) => setCheckoutDate(e.target.value)}
-          />
-          <label>Número de hóspedes</label>
-          <input
-            required="true"
-            id="numberOfGuests"
-            onChange={(e) => setNumberOfGuests(e.target.value)}
-          />
-          <label>Valor mínimo</label>
-          <input
-            id="minDailyRate"
-            onChange={(e) => setMinDailyRate(e.target.value)}
-          />
-          <label>Valor máximo</label>
-          <input
-            id="maxDailyRate"
-            onChange={(e) => setMaxDailyRate(e.target.value)}
-          />
-          <button className="button" type="submit">
-            Selecionar quarto(s)
-          </button>
-        </form>
+        <Formik validationSchema={Yup.object().shape({
+              checkinDate: Yup.date()
+                .required()
+                .min(new Date(2020, 9, 29), "AMIGA SUA LOKA"),
+              checkoutDate: Yup.date().required(),
+              numberOfGuests: Yup.number().required(),
+            })}>
+          <Form
+            onSubmit={handleRegister}
+          >
+            <label> Check-in</label>
+            <Field
+              name="checkinDate"
+              type="date"
+              placeholder="Check-in"
+              value={checkinDate}
+              onChange={(e) => setCheckinDate(e.target.value)}
+            />
+            <label> Check-in</label>
+            <Field
+              name="checkoutDate"
+              type="date"
+              placeholder="Check-out"
+              value={checkoutDate}
+              onChange={(e) => setCheckoutDate(e.target.value)}
+            />
+            <label> Check-in</label>
+            <Field
+              name="numberOfGuests"
+              value={numberOfGuests}
+              onChange={(e) => setNumberOfGuests(e.target.value)}
+            />
+            <button className="button" type="submit">
+              Selecionar quarto(s)
+            </button>
+            {/* <button type="submit">Submit</button>
+            <input
+              id="check-in"
+              type="date"
+              placeholder="Check-in"
+              value={checkinDate}
+              onChange={(e) => setCheckinDate(e.target.value)}
+            />
+            <label>Check-out</label>
+            <input
+              id="check-out"
+              type="date"
+              placeholder="Check-out"
+              value={checkoutDate}
+              onChange={(e) => setCheckoutDate(e.target.value)}
+            />
+            <label>Número de hóspedes</label>
+            <input
+              id="numberOfGuests"
+              onChange={(e) => setNumberOfGuests(e.target.value)}
+            />
+            <label>Valor mínimo</label>
+            <input
+              id="minDailyRate"
+              onChange={(e) => setMinDailyRate(e.target.value)}
+            />
+            <label>Valor máximo</label>
+            <input
+              id="maxDailyRate"
+              onChange={(e) => setMaxDailyRate(e.target.value)}
+            />
+            <button className="button" type="submit">
+              Selecionar quarto(s)
+            </button> */}
+          </Form>
+        </Formik>
       </div>
     </div>
   );
