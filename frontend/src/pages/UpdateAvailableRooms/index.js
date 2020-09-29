2386868231a708ffca76e1f47713cc3a8120019a
dandanscb <dandan.scb@hotmail.com>
2020-09-28 22:03:09 -0300

    Requisicao PUT para reservation

---
+++ frontend/src/pages/UpdateAvailableRooms/index.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import { AiFillPlusSquare, AiFillCheckSquare } from "react-icons/ai";
+
+import "./styles.css";
+
+import logoImg from "../../assets/images/logo.png";
+import api from "../../services/api";
+
+export default function UpdateAvailableRooms() {
+
+  const [rooms, setRoom] = useState([]);
+  const token = localStorage.getItem("token");
+  const checkinDate = localStorage.getItem("checkinDate");
+  const checkoutDate = localStorage.getItem("checkoutDate");
+  const numberOfGuests = localStorage.getItem("numberOfGuests");
+
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("api/rooms", {
+        headers: { Authorization: "Bearer " + token },
+        params: {
+          checkinDate,
+          checkoutDate,
+          numberOfGuests,
+        },
+      })
+      .then((response) => {
+        setRoom(response.data);
+      });
+  });
+
+  var [count, setCount] = useState(Array(rooms.length).fill(false));
+
+  function handleSelectRoom(id, index) {
+    const alreadySelected = selectedItems.findIndex((rooms) => rooms === id);
+
+    count.splice(index, 1, !count[index]);
+    setCount(count);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter((rooms) => rooms !== id);
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  }
+
+  return (
+    
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt="Logo" />
+        <span>Bem vindo ao Hostel</span>
+
+        <Link className="button" to="/payment/selectPayment">
+          Selecionar forma de pagamento
+        </Link>
+      </header>
+
+      <h1>Quartos Disponiveis</h1>
+
+      <ul>
+        {rooms.map((room, j) => (
+          <li key={room.id}>
+            <strong>QUARTO {room.number}:</strong>
+            <p>{room.description}</p>
+
+            <strong>DIMENSÃO:</strong>
+            <p>{room.dimension} m²</p>
+
+            <strong>LIMITE DE HÓSPEDES:</strong>
+            <p>{room.maxNumberOfGuests} pessoas</p>
+
+            <strong>VALOR:</strong>
+            <p>R$ {room.dailyRate.price},00</p>
+
+            
+            <button 
+            className={selectedItems.includes(room.id) ? "selected" : ""}
+            type="button" 
+            onClick={() => { handleSelectRoom(room.id, j) 
+              }}>
+              {count[j] ? <AiFillCheckSquare size="28px" color="#999" /> : <AiFillPlusSquare size="28px" color="#999" /> }
+            </button>
+
+          </li>
+        ))}
+        {localStorage.setItem("rooms_ID", JSON.stringify(selectedItems))}
+      </ul>
+    </div>
+  );
+}
---
+++ frontend/src/pages/UpdateReservation/index.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+
+import api from "../../services/api";
+import "./styles.css";
+
+import logoImg from "../../assets/images/logo.png";
+
+export default function UpdateReservation() {
+  const [checkinDate, setCheckinDate] = useState("");
+  const [checkoutDate, setCheckoutDate] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState("");
+
+  const token = localStorage.getItem("token");
+
+  const history = useHistory();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+
+    try {
+
+      localStorage.setItem('checkinDate', checkinDate);
+      localStorage.setItem('checkoutDate', checkoutDate);
+      localStorage.setItem('numberOfGuests', numberOfGuests);
+
+      console.log(checkinDate)
+      console.log(checkoutDate)
+      console.log(numberOfGuests)
+
+      if (checkinDate != null && checkoutDate != "") {
+        await api.get("api/rooms", {
+          headers: { Authorization: "Bearer " + token },
+          params: {
+            checkinDate,
+            checkoutDate,
+            numberOfGuests,
+          }
+        });
+  
+        history.push("/room/updateAvailableRooms");
+      } else {
+        history.push("/room/selectAvailableRooms");
+      }
+
+      
+    } catch (err) {
+      alert('Nenhum campo foi alterado!');
+      history.push("/profile");
+    }
+  }
+
+  return (
+    <div className="nova-reserva-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Logo" />
+
+          <h1>Editar reserva</h1>
+          <p>Altera sua reserva para melhor te atendermos!</p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041" />
+            Não tenho cadastro
+          </Link>
+        </section>
+
+        <form onSubmit={handleRegister}>
+          <label> Check-in</label>
+          <input
+            id="check-in"
+            type="date"
+            placeholder="Check-in"
+            value={checkinDate}
+            onChange={(e) => setCheckinDate(e.target.value)}
+          />
+          <label>Check-out</label>
+          <input
+            id="check-out"
+            type="date"
+            placeholder="Check-out"
+            value={checkoutDate}
+            onChange={(e) => setCheckoutDate(e.target.value)}
+          />
+          <label>Número de hóspedes</label>
+          <input
+            id="numberOfGuests"
+            onChange={(e) => setNumberOfGuests(e.target.value)}
+          />
+          <button className="button" type="submit">
+            Continuar com a alteração
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
