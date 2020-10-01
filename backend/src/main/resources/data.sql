cc9b6418c1fd00790014847840d7bcfaf1b0397d
vitormuuniz <vitormuuniz@gmail.com>
2020-09-30 23:55:18 -0300

    changing dimension room

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -7,8 +7,8 @@ INSERT INTO Customer(title, name, last_Name, birthday, address_ID, email, passwo
 insert into daily_rate(price) values (300), (120), (120), (200), (100);
 
 insert into room(description, number, dimension, max_number_of_guests, daily_rate_id) 
-	values 	('Quarto com uma cama de casal, duas camas de solteiro e varanda', 13, 460, 4, 1),
+	values 	('Quarto com uma cama de casal, duas camas de solteiro e varanda', 13, 500, 4, 1),
 			('Quarto com uma cama de casal', 14, 250, 2, 2),
-			('Quarto com uma cama de casal e duas camas de solteiro', 15, 230, 4, 3),
+			('Quarto com uma cama de casal e duas camas de solteiro', 15, 460, 4, 3),
 			('Quarto com uma cama de casal e com varanda', 16, 400, 2, 4),
 			('Quarto com duas camas de solteiro', 11, 230, 2, 5);
