f5f90bce765864fe137a91da81544e27300ed399
vitormuuniz <vitormuuniz@gmail.com>
2021-08-19 20:07:34 -0300

    Removing unused method

--- backend/src/test/java/br/com/hostel/tests/unit/guest/ListGuestsTest.java
+++ backend/src/test/java/br/com/hostel/tests/unit/guest/ListGuestsTest.java
@@ -7,15 +7,12 @@ import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -25,7 +22,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import br.com.hostel.exceptions.BaseException;
 import br.com.hostel.model.Address;
 import br.com.hostel.model.Guest;
-import br.com.hostel.model.Reservation;
 import br.com.hostel.model.helper.Role;
 import br.com.hostel.repository.AddressRepository;
 import br.com.hostel.repository.GuestRepository;
@@ -151,43 +147,4 @@ public class ListGuestsTest {
 		assertEquals(HttpStatus.NOT_FOUND, thrown.getHttpStatus());
 
 	}
-	
-	@Test
-	public void shouldThrowExceptionByFindGuestReservationsWithNonexistentID() throws Exception {
-		
-		Optional<Guest> opGuest = Optional.empty();
-		
-		when(guestRepository.findById(guest.getId())).thenReturn(opGuest);
-		
-		BaseException thrown = 
-				assertThrows(BaseException.class, 
-						() -> guestService.listGuestReservations(guest.getId()),
-						"It was expected that listGuestReservations() thrown an exception, " +
-						"due to trying to find a guest reservations with an nonexistent reservation ID");
-		
-		assertEquals(HttpStatus.NOT_FOUND, thrown.getHttpStatus());
-		
-	}
-	
-	@Test
-	public void shouldReturnAllGuestReservations() throws BaseException {
-		Reservation r1 = Mockito.mock(Reservation.class);
-		Reservation r2 = Mockito.mock(Reservation.class);
-		Reservation r3 = Mockito.mock(Reservation.class);
-		
-		Set<Reservation> reservationsList = new HashSet<>();
-		
-		reservationsList.add(r1);
-    	reservationsList.add(r2);
-    	reservationsList.add(r3);
-    	
-    	guest.setReservations(reservationsList);
-    	
-		when(guestRepository.findById(guest.getId())).thenReturn(Optional.of(guest));
-		
-		List<Reservation> reqReservationsList = guestService.listGuestReservations(guest.getId());
-		
-		assertEquals(3, reqReservationsList.size());
-	}
-	
 }
